e91e82d42e6870041747e19109c5a9908c55c5a1
exaltehq <exaltehq@gmail.com>
2022-01-28 20:49:30 -0500

    creds
    creds

--- setup/credentials.js
+++ setup/credentials.js
@@ -1,2 +1,2 @@
-export const SPACE_ID = "4nhaj6wzvnco";
-export const ACCESS_TOKEN = "F91A7b3FyjTFeH0sN6pYIfo6Nu1WZ2byX8Rdc4McGUI";
+export const SPACE_ID = "qlo8v233dr7r";
+export const ACCESS_TOKEN = "EFpwfG2MLILclI59O83oa8kh-PsEgPdlwOyf_DpTmFg";
